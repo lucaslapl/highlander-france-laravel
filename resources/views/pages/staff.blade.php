62dce81a66463ce18989f35cd1209723fd57b276
Shamp00 <lucas.laplanche77@gmail.com>
2026-09-07 10:12:50 +0200

    redesigned staff page

--- resources/views/pages/staff.blade.php
+++ resources/views/pages/staff.blade.php
@@ -6,111 +6,128 @@
 @section('content')
 
 <h1>L'équipe Highlander France</h1>
-<div id="staff">
+<div id="staff" class="staff-categories">
 
-    @if (!empty($groups['founders']))
-    <h3>Fondateurs</h3>
-    <hr>
-    <p>Les joueurs passionnés à l'initiative de ce projet !</p>
-    <div class="staff-role flex space-around align-center wrap">
-        @foreach ($groups['founders'] as $f)
-        <div class="staff-member">
-            <img loading="lazy" decoding="async" src="{!! e($f['avatar']) !!}" alt="Avatar de {!! e($f['final_name']) !!}">
-            <a href="{!! e($f['profile_url']) !!}">
-                <h4>{!! e($f['final_name']) !!}</h4>
-            </a>
-        </div>
-        @endforeach
-    </div>
-    @endif
-
-    @if (!empty($groups['moderators']))
-    <h3>Modération</h3>
-    <hr>
-    <p>L'équipe en charge du respect des règles et de la bonne ambiance au sein de la communauté.</p>
-    <div class="staff-role flex space-around align-center wrap">
-        @foreach ($groups['moderators'] as $m)
-        <div class="staff-member">
-            <img loading="lazy" decoding="async" src="{!! e($m['avatar']) !!}" alt="Avatar de {!! e($m['final_name']) !!}">
-            <a href="{!! e($m['profile_url']) !!}">
-                <h4>{!! e($m['final_name']) !!}</h4>
-            </a>
-        </div>
-        @endforeach
-    </div>
-    @endif
-
-    <h3>Twitch Highlander France</h3>
-    <hr>
-    <p>Les casters et l'équipe de production qui font vivre nos streams !</p>
-    <div class="staff-role twitch-staff">
-        @if (empty($groups['twitch']))
-            <p class="no-data">Aucun membre stream enregistré pour le moment.</p>
-        @else
-            @foreach ($groups['twitch'] as $tw)
-            <div class="staff-member">
-                <img loading="lazy" decoding="async" class="staff-pic" src="{!! e($tw['avatar']) !!}" alt="Avatar de {!! e($tw['final_name']) !!}">
-                <a href="{!! e($tw['profile_url']) !!}">
-                    <h4>{!! e($tw['final_name']) !!}</h4>
-                </a>
-                @if ((int) $tw['is_caster'] === 1 || (int) $tw['is_producer'] === 1)
-                <div class="twitch-badges">
-                    @if ((int) $tw['is_caster'] === 1)
-                    <span class="twitch-badge twitch-badge-caster">Caster</span>
-                    @endif
-                    @if ((int) $tw['is_producer'] === 1)
-                    <span class="twitch-badge twitch-badge-producer">Production</span>
-                    @endif
+    <article class="staff-category">
+        <h3>Fondateurs</h3>
+        <hr>
+        <p>Les joueurs passionnés à l'initiative de ce projet !</p>
+        <div class="staff-role">
+            @if (empty($groups['founders']))
+                <p class="no-data">Aucun fondateur enregistré pour le moment.</p>
+            @else
+                @foreach ($groups['founders'] as $f)
+                <div class="staff-member">
+                    <img loading="lazy" decoding="async" src="{!! e($f['avatar']) !!}" alt="Avatar de {!! e($f['final_name']) !!}">
+                    <div class="staff-name-group">
+                        <a href="{!! e($f['profile_url']) !!}">
+                            <h4>{!! e($f['final_name']) !!}</h4>
+                        </a>
+                    </div>
                 </div>
-                @endif
-            </div>
-            @endforeach
-        @endif
-    </div>
+                @endforeach
+            @endif
+        </div>
+    </article>
 
-    <div id="sous-staff" class="flex space-around">
+    <article class="staff-category">
+        <h3>Modération</h3>
+        <hr>
+        <p>L'équipe en charge du respect des règles et de la bonne ambiance.</p>
+        <div class="staff-role">
+            @if (empty($groups['moderators']))
+                <p class="no-data">Aucun modérateur enregistré pour le moment.</p>
+            @else
+                @foreach ($groups['moderators'] as $m)
+                <div class="staff-member">
+                    <img loading="lazy" decoding="async" src="{!! e($m['avatar']) !!}" alt="Avatar de {!! e($m['final_name']) !!}">
+                    <div class="staff-name-group">
+                        <a href="{!! e($m['profile_url']) !!}">
+                            <h4>{!! e($m['final_name']) !!}</h4>
+                        </a>
+                    </div>
+                </div>
+                @endforeach
+            @endif
+        </div>
+    </article>
 
-        <div id="mentors">
-            <h3>Mentors</h3>
-            <hr>
-            <p>Les joueurs expérimentés qui accompagnent les nouveaux venus dans leur progression en compétitif !</p>
-            <div class="staff-role">
-                @if (empty($groups['mentors']))
-                    <p class="no-data">Aucun mentor enregistré pour le moment.</p>
-                @else
-                    @foreach ($groups['mentors'] as $me)
-                    <div class="staff-member flex align-center">
-                        <img loading="lazy" decoding="async" class="staff-pic" src="{!! e($me['avatar']) !!}" alt="Avatar de {!! e($me['final_name']) !!}">
+    <article class="staff-category">
+        <h3>Mentors</h3>
+        <hr>
+        <p>Les joueurs expérimentés qui accompagnent les nouveaux venus.</p>
+        <div class="staff-role">
+            @if (empty($groups['mentors']))
+                <p class="no-data">Aucun mentor enregistré pour le moment.</p>
+            @else
+                @foreach ($groups['mentors'] as $me)
+                <div class="staff-member">
+                    <img loading="lazy" decoding="async" src="{!! e($me['avatar']) !!}" alt="Avatar de {!! e($me['final_name']) !!}">
+                    <div class="staff-name-group">
                         <a href="{!! e($me['profile_url']) !!}">
                             <h4>{!! e($me['final_name']) !!}</h4>
                         </a>
                     </div>
-                    @endforeach
-                @endif
-            </div>
+                </div>
+                @endforeach
+            @endif
         </div>
+    </article>
 
-        <div id="mixers">
-            <h3>Lanceurs de mix</h3>
-            <hr>
-            <p>Les joueurs qui organisent les mixs pour permettre à tous de jouer en compétitif dans une ambiance conviviale !</p>
-            <div class="staff-role">
-                @if (empty($groups['mixers']))
-                    <p class="no-data">Aucun lanceur de mix enregistré pour le moment.</p>
-                @else
-                    @foreach ($groups['mixers'] as $mi)
-                    <div class="staff-member flex align-center">
-                        <img loading="lazy" decoding="async" class="staff-pic" src="{!! e($mi['avatar']) !!}" alt="Avatar de {!! e($mi['final_name']) !!}">
+    <article class="staff-category">
+        <h3>Lanceurs de mix</h3>
+        <hr>
+        <p>Les joueurs qui organisent les mixs dans une ambiance conviviale !</p>
+        <div class="staff-role">
+            @if (empty($groups['mixers']))
+                <p class="no-data">Aucun lanceur de mix enregistré pour le moment.</p>
+            @else
+                @foreach ($groups['mixers'] as $mi)
+                <div class="staff-member">
+                    <img loading="lazy" decoding="async" src="{!! e($mi['avatar']) !!}" alt="Avatar de {!! e($mi['final_name']) !!}">
+                    <div class="staff-name-group">
                         <a href="{!! e($mi['profile_url']) !!}">
                             <h4>{!! e($mi['final_name']) !!}</h4>
                         </a>
                     </div>
-                    @endforeach
-                @endif
-            </div>
+                </div>
+                @endforeach
+            @endif
         </div>
+    </article>
 
-    </div>
+    <article class="staff-category">
+        <h3>Twitch Highlander France</h3>
+        <hr>
+        <p>Les casters et l'équipe de production qui font vivre nos streams !</p>
+        <div class="staff-role">
+            @if (empty($groups['twitch']))
+                <p class="no-data">Aucun membre stream enregistré pour le moment.</p>
+            @else
+                @foreach ($groups['twitch'] as $tw)
+                <div class="staff-member">
+                    <img loading="lazy" decoding="async" src="{!! e($tw['avatar']) !!}" alt="Avatar de {!! e($tw['final_name']) !!}">
+                    <div class="staff-name-group">
+                        <a href="{!! e($tw['profile_url']) !!}">
+                            <h4>{!! e($tw['final_name']) !!}</h4>
+                        </a>
+                        @if ((int) $tw['is_caster'] === 1 || (int) $tw['is_producer'] === 1)
+                        <div class="twitch-badges">
+                            @if ((int) $tw['is_caster'] === 1)
+                            <span class="twitch-badge twitch-badge-caster">Caster</span>
+                            @endif
+                            @if ((int) $tw['is_producer'] === 1)
+                            <span class="twitch-badge twitch-badge-producer">Production</span>
+                            @endif
+                        </div>
+                        @endif
+                    </div>
+                </div>
+                @endforeach
+            @endif
+        </div>
+    </article>
+
 </div>
 
 @push('scripts')
